77f3bd8d323cb9722525c10750ec22c152465f3c
dwajsman <danielwajsman@mac.com>
2021-10-25 22:08:42 -0300

    version after lint

--- lunch.js
+++ lunch.js
@@ -15,23 +15,21 @@
 
 const whatToDoForLunch = function(hungry, availableTime) {
   
-  if (hungry == false) {console.log("Wait until you're hungry"); return;}
-  if (availableTime < 20) {console.log("Pick something up and eat in back in the Lab or in the kitchen, where you can get to know your fellow classmatesinue"); return;}
-  if (availableTime < 30) {console.log("You deserve a break. Try a place in Gastown"); return;}
+  if (hungry === false) {
+    console.log("Wait until you're hungry");
+    return;
+  }
+  if (availableTime < 20) {
+    console.log("Pick something up and eat in back in the Lab or in the kitchen, where you can get to know your fellow classmatesinue");
+    return;
+  }
+  if (availableTime < 30) {
+    console.log("You deserve a break. Try a place in Gastown");
+    return;
+  }
   console.log("You should reconsider your plans");
     
-  }
-
-  // If you have less than 20 minutes, you want your program to tell you to pick something up and eat in back in the Lab or in the kitchen, where you can get to know your fellow classmates.
-  // If you have at least 20 minutes but no more than 30 (because you've been working hard and are ahead of schedule), you want your program to tell you that you deserve a break and could try a place in Gastown.
-  // If you have more than 30 minutes, you want your program to remind you that this is a bootcamp after all and you should probably reconsider.
-
-
-
-
-
-
-
+};
 
 
 
